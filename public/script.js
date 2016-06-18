180fb26f9d5b4c355e4647770696d50b9634ce7b
Guelmis Cortina <cguelmis@yahoo.com>
2016-06-17 20:26:55 -0400

    Add smooth scrolling effect to the window. Add active class effect to the navbar

--- public/script.js
+++ public/script.js
@@ -15,4 +15,33 @@ $(document).ready(function(){
 	    // window width is less than 991px
 	  }
 	}
+
+	// localScroll
+	$.localScroll({duration:800, hash:true});
+
+	// Navigation Active Class
+	var firstSlide = $("#first-slide").offset().top;
+	var secondSlide = $("#second-slide").offset().top;
+	var thirdSlide = $("#third-slide").offset().top;
+	var fourthSlide = $("#fourth-slide").offset().top;
+	var fithSlide = $("#fith-slide").offset().top;
+
+	var $w = $(window).scroll(function(){
+		$( '.nav li a' ).each(function( index ) {
+			console.log($(this).text());
+			$(this).removeClass('active');
+		});
+		if ( ($w.scrollTop() >= firstSlide) && ($w.scrollTop() < secondSlide)){
+			$('#menu-home').addClass('active');
+		}else if ( ($w.scrollTop() >= secondSlide) && ($w.scrollTop() < thirdSlide)){
+			$('#menu-about').addClass('active');
+		}else if ( ($w.scrollTop() >= thirdSlide) && ($w.scrollTop() < fourthSlide)){
+			$('#menu-portfolio').addClass('active');
+		}else if ( ($w.scrollTop() >= fourthSlide) && ($w.scrollTop() < fithSlide)){
+			$('#menu-education').addClass('active');
+		}else{
+			$('#menu-contact').addClass('active');
+		}
+	});
+
 });
